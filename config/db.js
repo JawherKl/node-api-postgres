861c0257e61afbdbf98704f108ee901f24f2bb56
jawher.kallel <jawher.kallel@key-performance-group.com>
2024-12-26 09:55:35 +0100

    hosting database testing to neon

--- config/db.js
+++ config/db.js
@@ -2,11 +2,20 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DB_USER,
+  /*user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_DATABASE,
   password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT,*/
+  connectionString: process.env.DATABASE_URL,
+});
+
+pool.on('connect', () => {
+  console.log('Connected to the database.');
+});
+
+pool.on('error', (err) => {
+  console.error('Database connection error:', err);
 });
 
 module.exports = pool;
